06c3e33fc32a7179dacc0f7fc450d4721e1639ea
jelizasilatei <silateijc90@gmail.com>
2023-03-13 00:12:37 +0300

    DOM modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let body = document.querySelector("body");
+let main = document.querySelector("main#main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Jeliza is the champion";
+body.append(newHeader);
